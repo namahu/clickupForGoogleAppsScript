55f62895e77347216b3a6b65e23c7ed5362349de
namahu <neti0326@gmail.com>
2020-10-31 15:14:44 +0900

    Remove unnecessary code

--- src/Clickup.ts
+++ src/Clickup.ts
@@ -13,7 +13,7 @@ const getClickup = (apiToken: string): Clickup => {
   return new Clickup(apiToken);
 };
 
-export default class Clickup {
+class Clickup {
   readonly apiToken: string;
   readonly baseURL: string = "https://api.clickup.com/api/v2/";
   _request: ClickupRequest_;
@@ -25,35 +25,4 @@ export default class Clickup {
     this.apiToken = apiToken;
     this._request = new ClickupRequest_(this.baseURL, apiToken);
   }
-
-  /**
-   * Get a specific list of tasks
-   *
-   * @param listId - ID of list to get task
-   * @param archive - Flag to include archived tasks
-   */
-  // getTasksByListId(listId: string, archived: boolean = false) {
-  //   const path: string = `list/${listId}/task?archived=${archived}`;
-  //   return this._request.get_(path);
-  // }
-
-  // getTaskByTaskId(taskId: string) {
-  //   const path: string = `task/${taskId}`;
-  //   return this._request.get_(path);
-  // }
-
-  // createTaskInList(listId: string, payLoad) {
-  //   const path: string = `list/${listId}/task`;
-  //   return this._request.post_(payLoad, path);
-  // }
-
-  // updateTaskByTaskId(taskId: string, payLoad) {
-  //   const path: string = `task/${taskId}`;
-  //   return this._request.put_(payLoad, path);
-  // }
-
-  // deleteTaskByTaskId(taskId: string) {
-  //   const path: string = `task/${taskId}`;
-  //   return this._request.delete_(path);
-  // }
-}
+};
